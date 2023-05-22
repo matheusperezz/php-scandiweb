7f72e4fb7d5a9c849a635571a0deae2e4b6825bd
matheusperezz <matheus.mperez08@gmail.com>
2023-05-22 19:27:42 -0300

    refactor on ProductController and Index

--- index.php
+++ index.php
@@ -20,6 +20,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
+// Autoloading classes
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    require_once "$class.php";
+});
 
 // Init database
 $servername = "us-cdbr-east-06.cleardb.net";
@@ -35,10 +40,8 @@ $productController = new ProductController($conn);
 // ENDPOINT (GET) '/'
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] == '/') {
     try {
-
         $results = $productController->getAllProducts();
         echo json_encode($results);
-
     } catch (\PDOException $e) {
         echo "Error in connection: {$e->getMessage()}";
     }
@@ -46,29 +49,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] == '/') {
 
 // ENDPOINT (POST) '/'
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
     $data = json_decode(file_get_contents('php://input'), true);
 
-    if (!empty($data['SKU']) && !empty($data['name']) && !empty($data['price'])) {
+    $product = null;
 
+    if (!empty($data['SKU']) && !empty($data['name']) && !empty($data['price'])) {
         if (!empty($data['weight'])) {
-            $book = new Book($data['SKU'], $data['name'], $data['price'], $data['weight']);
-            $productController->saveBook($book);
-            echo json_encode("Book saved.");
+            $product = new Book($data['SKU'], $data['name'], $data['price'], $data['weight']);
         } else if (!empty($data['size'])) {
-            $dvd = new Dvd($data['SKU'], $data['name'], $data['price'], $data['size']);
-            $productController->saveDvd($dvd);
-            echo json_encode("Dvd saved.");
+            $product = new Dvd($data['SKU'], $data['name'], $data['price'], $data['size']);
         } else if (!empty($data['height']) && !empty($data['width']) && !empty($data['length'])) {
-            $furniture = new Furniture($data['SKU'], $data['name'], $data['price'], $data['height'], $data['width'], $data['length']);
-            $productController->saveFurniture($furniture);
-            echo json_encode("Furniture saved.");
+            $product = new Furniture($data['SKU'], $data['name'], $data['price'], $data['height'], $data['width'], $data['length']);
         }
 
+        if ($product !== null) {
+            $productController->saveProduct($product);
+            echo json_encode("Product saved.");
+        } else {
+            http_response_code(400);
+            echo json_encode("Invalid product data");
+        }
     } else {
         http_response_code(400);
         echo json_encode("Invalid input");
-
     }
 }
 
